17a3be33372eea42c4f4e2b5d73fdf06cb3f27ba
Thomas Chen <chen.thomas.c@gmail.com>
2015-08-18 18:16:29 -0400

    Improve UI
    - When band is created, transitions to song listing for that band
    - Add blurb to nudge user to create first song if song list is empty
    - When users clicks on blurb link to create new song, text input field
    displays

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   title: '',
+  songCreationStarted: false,
 
   isAddButtonDisabled: Ember.computed('title', function() {
     return Ember.isEmpty(this.get('title'));
+  }),
+  noSongs: Ember.computed('model.songs.length', function() {
+    return this.get('model.songs.length') === 0;
+  }),
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
 
   actions: {
@@ -13,6 +20,9 @@ export default Ember.Controller.extend({
       rating = params.rating;
 
       song.set('rating', rating);
+    },
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
     }
   }
 });
